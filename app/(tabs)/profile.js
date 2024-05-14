667733d74570585d881cded9778308ccddcd8dd9
reinhartlim <18221154@std.stei.itb.ac.id>
2024-05-14 14:09:59 +0700

    feat: add profile screen

--- app/(tabs)/profile.js
+++ app/(tabs)/profile.js
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import ProfileNavigation from "../../components/ProfileNavigation";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <View className="bg-green">
+        <View className="h-2/5 bg-green items-center">
+          <View className="justify-center items-center">
+            <Text className="mt-[6px] text-white font-psemibold text-[20px]">
+              Akun
+            </Text>
+          </View>
+          <View className="mt-[20px] h-[130px] w-[130px] bg-white rounded-full"></View>
+          <View className="mt-[24px] items-center">
+            <Text className="font-psemibold text-[18px] text-white">
+              John Doe
+            </Text>
+            <Text className="font-pregular text-[13px] text-white">
+              Bandung, West Java
+            </Text>
+          </View>
+        </View>
+        <View className="bg-white rounded-t-[30px] h-3/5">
+          <View className="justify-center items-center">
+            <View className="mt-[14px] w-[70px] h-[4px] flex flex-row">
+              <View className="bg-orange flex-1"></View>
+            </View>
+          </View>
+          <View className="mt-[11px] mx-[33px]">
+            <Text className="font-psemibold text-[14px] opacity-50">
+              General
+            </Text>
+            <ProfileNavigation />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Profile
+export default Profile;
---
+++ components/ProfileNavigation.js
@@ -0,0 +1,50 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { images, icons } from '../constants'
+
+const data = [
+  {
+    id: 1,
+    name: 'Profile',
+    icon: icons.User,
+  },
+  {
+    id: 2,
+    name: 'Preferensi',
+    icon: icons.Preference,
+  },
+  {
+    id: 3,
+    name: 'Digital Pantry',
+    icon: icons.DigitalPantry,
+  },
+  {
+    id: 4,
+    name: 'Pengaturan',
+    icon: icons.Settings,
+  },
+  {
+    id: 5,
+    name: 'Log Out',
+    icon: icons.LogOut
+  }
+]
+
+const ProfileNavigation = () => {
+  return (
+    <View>
+      {data.map((item) => (
+        <TouchableOpacity key={item.id}>
+          <View className="flex flex-row items-center justify-between py-4 border-b-[0.75px] border-b-[#D9D9D9]">
+            <View className="flex flex-row items-center">
+              <Image source={item.icon} className="w-[32px] h-[32px]" />
+              <Text className="font-pmedium text-[14px] ml-4">{item.name}</Text>
+            </View>
+            <Image source={icons.RightChevron} className="w-[20px] h-[20px]" />
+          </View>
+        </TouchableOpacity>
+      ))}
+    </View>
+  )
+}
+
+export default ProfileNavigation
